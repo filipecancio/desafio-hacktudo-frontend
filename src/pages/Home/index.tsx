9725040dfd274b2ef015c53dbcc48ad906be9969
Daniel Correia <danncorreia95@hotmail.com>
2021-10-22 13:51:19 -0300

    inicio da criação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,71 @@
 import React from 'react';
-import { Container } from './styles';
+import { Text } from 'react-native';
+import favIcon from '../../assets/icons/fav.png';
+import filterIcon from '../../assets/icons/filter.png';
+import searchIcon from '../../assets/icons/search.png';
+import {
+  Container,
+  Header,
+  GroupIcons,
+  ImgIcon,
+  ToolBar,
+  FilterBar,
+  FilterBarItem,
+  List,
+  ListItem,
+  ImgItem,
+  ListItemDescription,
+} from './styles';
 
 export const Home: React.FC = () => (
   <Container>
-    <h1>Page Home</h1>
+    <Header>
+      <ToolBar>
+        <ImgIcon src={filterIcon} alt="logo" />
+        <GroupIcons>
+          <ImgIcon src={searchIcon} alt="logo" />
+          <ImgIcon src={favIcon} alt="logo" />
+        </GroupIcons>
+      </ToolBar>
+      <FilterBar>
+        <FilterBarItem>
+          DDD 77-
+        </FilterBarItem>
+        <FilterBarItem>
+          Categoria
+        </FilterBarItem>
+        <FilterBarItem>
+          Filtros
+        </FilterBarItem>
+      </FilterBar>
+    </Header>
+    <List>
+      <ListItem>
+        <ImgItem src="https://a-static.mlcdn.com.br/1500x1500/celular-samsung-galaxy-a-02-s-32gb-dual-sm-a025mzkvzto/gazinshop/9589/052ae7ff8a008d1e9724c2b6e9f71862.jpg" alt="logo Filtros" />
+        <ListItemDescription>
+          <Text>Titulo do Produto</Text>
+          <Text>R$0.00</Text>
+          <Text>16 ourutbor 21:05, Candeias</Text>
+        </ListItemDescription>
+      </ListItem>
+
+      <ListItem>
+        <ImgItem src="https://a-static.mlcdn.com.br/1500x1500/celular-samsung-galaxy-a-02-s-32gb-dual-sm-a025mzkvzto/gazinshop/9589/052ae7ff8a008d1e9724c2b6e9f71862.jpg" alt="logo Filtros" />
+        <ListItemDescription>
+          <Text>Titulo do Produto</Text>
+          <Text>R$0.00</Text>
+          <Text>16 ourutbor 21:05, Candeias</Text>
+        </ListItemDescription>
+      </ListItem>
+
+      <ListItem>
+        <ImgItem src="https://a-static.mlcdn.com.br/1500x1500/celular-samsung-galaxy-a-02-s-32gb-dual-sm-a025mzkvzto/gazinshop/9589/052ae7ff8a008d1e9724c2b6e9f71862.jpg" alt="logo Filtros" />
+        <ListItemDescription>
+          <Text>Titulo do Produto</Text>
+          <Text>R$0.00</Text>
+          <Text>16 ourutbor 21:05, Candeias</Text>
+        </ListItemDescription>
+      </ListItem>
+    </List>
   </Container>
 );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,80 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    height: 100%;
+    width: 100%;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 15px;
+`;
+
+export const ToolBar = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-end;
+    color: black;
+    height: 65px;
+    background: #219653;
+    padding: 23px;
+`;
+
+export const FilterBar = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: flex-end;
+    backgrond:#EFEFEF;
+    color: black;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const FilterBarItem = styled.button`
+    font-family: Arial;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    color: #204628;
+
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    backgrond:#EFEFEF;
+    width: 100%;
+    padding: 15px 0;
+`;
+
+export const ImgIcon = styled.img`
+`;
+
+export const GroupIcons = styled.div`
+    display:flex;
+    gap: 15px;
+    display: flex;
+    align-items: flex-end;
+`;
+
+export const List = styled.div`
+    height: 10px;
+`;
+
+export const ListItem = styled.div`
+    height: 134px;
+    display:flex;
+    margin-bottom: 5px;
+`;
+
+export const ImgItem = styled.img`
+`;
+
+export const ListItemDescription = styled.div`
+    padding: 10px;
+    display:flex;
+    flex-direction: column;
+    justify-content:space-between;
+    align-items: flex-start;
+`;
